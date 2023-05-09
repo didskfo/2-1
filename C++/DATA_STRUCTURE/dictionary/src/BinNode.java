3b028ea532389b4061c836cc11b67264861b1b9d
didskfo <101495452+didskfo@users.noreply.github.com>
2023-05-09 16:41:32 +0900

    Delete C++/DATA_STRUCTURE directory

--- C++/DATA_STRUCTURE/dictionary/src/BinNode.java
+++
@@ -1,9 +0,0 @@
-public interface BinNode<E> {
-    public E element();
-    public void setElement(E v);
-    public BinNode<E> left();
-    public BinNode<E> right();
-    public boolean isLeaf();
-    public void setLeft(BinNode<E> node);
-    public void setRight(BinNode<E> node);
-}
--- C++/DATA_STRUCTURE/ds_hw1/list/ListIterator.java
+++
@@ -1,8 +0,0 @@
-package ds_hw1.list;
-
-public interface ListIterator<E> {
-	boolean hasNext();
-	E next();
-	boolean hasPrevious();
-	E previous();
-}
